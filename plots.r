56327a77011e38648cd73210124d87447fe13852
Ran <ransnit@gmail.com>
2014-11-17 21:39:45 +0200

    Extensions

--- plots.r
+++ plots.r
@@ -1,5 +1,8 @@
-id <- function(x){return(x)}
-
-plot.function(id, from = 0, to = 1, lty = "dashed")
-
-segments(x0 = 0, y0 = 0, x1 = 0.5, y1 = 0)
+plot.best.response <- function(p)
+{
+  id <- function(x){return(x)}
+  plot.function(id, from = 0, to = 1, lty = "dashed")
+  segments(x0 = 0, y0 = 0, x1 = p, y1 = 0)
+  segments(x0 = p, y0 = 0, x1 = p, y1 = 1)
+  segments(x0 = p, y0 = 1, x1 = 1, y1 = 1)
+}
