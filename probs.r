d59ff0cbc2d626a43f7cbe6c879d4fc9970cdad2
Ran <ransnit@gmail.com>
2014-12-12 23:32:15 +0200

    probs

--- probs.r
+++ probs.r
@@ -11,17 +11,13 @@ calc.prob.table.by.p00 <- function(ro, p, n)
   d[1,] <- c(1, 0, -1, t, NA, NA)
   d[1, S_n.l] <- d[1, P_n1.l] - ro*p*d[1, P_n0.l]
   
-  for(i in 2:n)
+  for(i in 1:(n-1))
   {
-    d[i, P_n1.l] <- ro*d[i-1, P_n1.l] + d[i-1, S_n.l]
-    d[i, P_n0.l] <- (1-p)*ro*d[i-1, P_n0.l] - d[i-1, S_n.l]
-    d[i, S_n.l] <- d[i-1, S_n.l] + d[i, P_n1.l] - ro*p*d[i, P_n0.l]
+    d[i+1, P_n1.l] <- ro*d[i, P_n1.l] + d[i, S_n.l]
+    d[i+1, P_n0.l] <- (1-p)*ro*d[i, P_n0.l] - d[i, S_n.l]
+    d[i+1, S_n.l] <- d[i, S_n.l] + d[i+1, P_n1.l] - ro*p*d[i+1, P_n0.l]
   }
   
-#   a <- sum(d$P_n0_a, d$P_n1_a)
-#   b <- sum(d$P_n0_b, d$P_n1_b)
-#   
-#   P_00 <- (1 - b) / a
   return (d)
 }
 
@@ -36,10 +32,12 @@ calc.p00.from.table <- function(d)
 calc.prob.table <- function(d)
 {
   p00 <- calc.p00.from.table(d)
-  t <- data.frame(matrix(NA, nrow(d), 2))
-  names(t) <- c("P_n0", "P_n1")
   
-  # TODO: continue from here
+  P_n0 <- d$P_n0_a*p00 + d$P_n0_b
+  P_n1 <- d$P_n1_a*p00 + d$P_n1_b
+
+  t <- data.frame(cbind(P_n0, P_n1))
+  names(t) <- c("P_n0", "P_n1")
   
   return (t)
 }
